c16006693b006ace7b2664178192bc56d5b26a40
Thilini_Fernando <tpfernando.94@gmail.com>
2017-10-24 00:26:58 +0530

    Customer view prof

--- application/views/Customer_Profile/CustProfile.php
+++ application/views/Customer_Profile/CustProfile.php
@@ -1,23 +1,41 @@
 <?php include 'HnF/cus_header.php' ?>
-
-	<div id="container">
-		<center>
-			<h1><b>Customer Profile</b></h1>
-
-    		<!--?php echo 'Customer id: '.$this->session->userdata('user_id');?>
-            <!--?php echo 'All: '.$user_det;?-->
-
-            <br>
-    		<?php
-
-    		$cur_date = date('Y-m-d');
-    		echo 'Current date: '.$cur_date;
-
-    		?>
+    <div class="right">
+    <div class="container">
 
 
-			
-		</center>
-	</div>
+
+
+                    <?php
+
+                    $cur_date = date('Y-m-d');
+                    echo 'Current date: '.$cur_date;
+
+                    ?>
+                    <center>
+                        <h1><b>Customer Profile</b></h1>
+                        <hr>
+                        <table align="center">
+                            <tr><td width="40%"> <h4><?php echo  'First Name: <td>'.$this->session->userdata('fname');?></h4><br></td></tr>
+                            <tr><td><h4><?php echo   'Last Name: <td > '.$this->session->userdata('lname');?></h4><br></td></tr>
+                            <tr><td> <h4> <?php echo 'Address:<td> '.$this->session->userdata('address');?></h4><br></td></tr>
+                            <tr><td> <h4> <?php echo 'Contact No:<td> '.$this->session->userdata('contact');?> </h4><br></td></tr>
+                            <tr><td><h4><?php echo   'email:<td> '.$this->session->userdata('email');?></h4></td></tr>
+
+                            <br>
+
+
+            </table>
+        </div>
+    </div>
+
+
+
+    <!--?php echo 'All: '.$user_det;?-->
+
+
+
+
+    </center>
+
 
 <?php include 'HnF/cus_footer.php' ?>
